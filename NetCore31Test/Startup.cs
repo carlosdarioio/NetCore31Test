42fcbaa99495cbcfcfd4dbce8c2e8f9fd35bb85d
Carlos Dario Flores <cdfn3@hotmail.com>
2021-11-16 20:26:04 -0600

    make change to master

--- NetCore31Test/Startup.cs
+++ NetCore31Test/Startup.cs
@@ -15,22 +15,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-/*
- //CLIENTES CONTROLLER FUNCIONAL VAS POR 3 Crear CRUD para tabla Póliza
- https://www.syncfusion.com/blogs/post/how-to-build-crud-rest-apis-with-asp-net-core-3-1-and-entity-framework-core-create-jwt-tokens-and-secure-apis.aspx
-Create an empty API controller called TokenController.!!!
-    
- alola x auqi vas
-Create an empty API controller called TokenController.
 
-
-que simple crera todo auto con ejecutando en la terminal
-Scaffold-DbContext "Server=DESKTOP-1ULHC37;
-Database=Inventory;Integrated Security=True" 
-Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
-En un alinea=
-Scaffold-DbContext "Server=DESKTOP-1ULHC37;Database=Inventory;Integrated Security=True" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
- */
 namespace NetCore31Test
 {
     public class Startup
@@ -44,8 +29,7 @@ namespace NetCore31Test
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            //alola
+        {            
             var connection = Configuration.GetConnectionString("InventoryDatabase");            
             services.AddDbContextPool<InventoryContext>(options => options.UseSqlServer(connection));
             services.AddControllers();
